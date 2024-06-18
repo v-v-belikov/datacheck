ec9008752738dd456ad91265440d62fd1c6676be
Vladimir Belikov <v.v.belikov.work@gmail.com>
2024-06-18 17:47:29 +0400

    fix

--- src/pages/GeneralManagerPage.tsx
+++ src/pages/GeneralManagerPage.tsx
@@ -416,7 +416,7 @@ export const GeneralManagerPage = () => {
   const [value, setValue] = React.useState(0);
 
 
-  const handleChange = (event: React.SyntheticEvent, newValue: number) => {
+  const handleChange = (_event: React.SyntheticEvent, newValue: number) => {
     setValue(newValue);
   };
   const [rows, setRows] = useState(initialRows)

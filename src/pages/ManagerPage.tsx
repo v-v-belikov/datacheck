1bf53b950815df1f143ecafe684fdf1394dc5d79
Vladimir Belikov <v.v.belikov.work@gmail.com>
2024-06-18 16:36:21 +0400

    add title

--- src/pages/ManagerPage.tsx
+++ src/pages/ManagerPage.tsx
@@ -71,6 +71,7 @@ export const ManagerPage: React.FC = () => {
 
   return (
     <div style={{flexBasis: "50%", width: "750px", paddingTop: "50px"}}>
+      <h1>Приоритеты</h1>
       <DndContext sensors={sensors} collisionDetection={closestCenter} onDragEnd={handleDragEnd}>
         <SortableContext items={rows} strategy={verticalListSortingStrategy}>
           <TableContainer component={Paper}>

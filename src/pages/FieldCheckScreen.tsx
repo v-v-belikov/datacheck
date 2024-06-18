f391c4ee8bc9abb3e8b874c313ad174eee722b0b
Vladimir Belikov <v.v.belikov.work@gmail.com>
2024-06-18 16:58:13 +0400

    add buttons

--- src/pages/FieldCheckScreen.tsx
+++ src/pages/FieldCheckScreen.tsx
@@ -55,26 +55,31 @@ export const FieldCheckScreen: React.FC<Props> = ({
   // }
 
   return (
-    <div style={{display: "flex", width: "80vw", height: "85vh", objectFit: "contain"}}>
-      <div style={{width: "50%", display: "flex", alignItems: "center", justifyContent: "center", padding: "50px"}}>
-        {/* <img src={docs[docIndex].image} style={{height: "100%"}}/> */}
-        <ImageViewer
-          src={scanSrc}
-        />
-      </div>
-      <div style={{width: "50%", display: "flex", flexDirection: "column", alignItems: "center", justifyContent: "center"}}>
-        <div>
-          <div style={{fontSize: "18px", fontWeight: "bold", marginBottom: "15px"}}>
-            {field.title}
-          </div>
-          <TextField onChange={(evt) => {
-              onFieldValueChanged(evt.target.value)
-          }} value={field.value} variant="outlined" style={{marginBottom: "15px", width: "100%"}} focused fullWidth/>
-          <div style={{display: "flex"}}>
-            <Button onClick={onNextField} variant="contained" style={{width: "160px", backgroundColor: "green", padding: "15px 30px", marginRight: "15px", fontSize: "16px"}}>Ок</Button>
-            <Button onClick={onNextForm} variant="contained" style={{width: "160px", backgroundColor: "red", padding: "15px 30px", fontSize: "16px"}}>Не ок</Button>
+    <div>
+      <div style={{display: "flex", width: "80vw", height: "85vh", objectFit: "contain"}}>
+        <div style={{width: "50%", display: "flex", alignItems: "center", justifyContent: "center", padding: "50px"}}>
+          {/* <img src={docs[docIndex].image} style={{height: "100%"}}/> */}
+          <ImageViewer
+            src={scanSrc}
+          />
+        </div>
+        <div style={{width: "50%", display: "flex", flexDirection: "column", alignItems: "center", justifyContent: "center"}}>
+          <div>
+            <div style={{fontSize: "18px", fontWeight: "bold", marginBottom: "15px"}}>
+              {field.title}
+            </div>
+            <TextField onChange={(evt) => {
+                onFieldValueChanged(evt.target.value)
+            }} value={field.value} variant="outlined" style={{marginBottom: "15px", width: "100%"}} focused fullWidth/>
+            <div style={{display: "flex"}}>
+              <Button onClick={onNextField} variant="contained" style={{width: "160px", backgroundColor: "green", padding: "15px 30px", marginRight: "15px", fontSize: "16px"}}>Ок</Button>
+              <Button onClick={onNextForm} variant="contained" style={{width: "160px", backgroundColor: "red", padding: "15px 30px", fontSize: "16px"}}>Не ок</Button>
+            </div>
           </div>
         </div>
+      </div>
+      <div style={{display: "flex", justifyContent: "flex-end"}}>
+        <Button variant="contained">Завершить смену</Button>
       </div>
     </div>
   )
--- src/pages/PhotoCheckScreen.tsx
+++ src/pages/PhotoCheckScreen.tsx
@@ -10,31 +10,36 @@ interface Props {
 
 export const PhotoCheckScreen: React.FC<Props> = ({onNextStep, onNextForm, scanSrc, photoSrc}) => {
   return (
-    <div style={{display: "flex", flexDirection: "column", alignItems: "center"}}>
-      <div style={{display: "flex", width: "80vw", height: "80%", objectFit: "contain", justifyContent: "center", marginBottom: "0"}}>
-        <div style={{width: "40%", display: "flex", alignItems: "center", justifyContent: "center", padding: "20px"}}>
-          {/* <img src={docs[docIndex].image} style={{height: "100%"}}/> */}
-          <ImageViewer
-            src={scanSrc}
-          />
+    <div>
+      <div style={{display: "flex", flexDirection: "column", alignItems: "center"}}>
+        <div style={{display: "flex", width: "80vw", height: "80%", objectFit: "contain", justifyContent: "center", marginBottom: "0"}}>
+          <div style={{width: "40%", display: "flex", alignItems: "center", justifyContent: "center", padding: "20px"}}>
+            {/* <img src={docs[docIndex].image} style={{height: "100%"}}/> */}
+            <ImageViewer
+              src={scanSrc}
+            />
+          </div>
+          <div style={{width: "40%", display: "flex", alignItems: "center", justifyContent: "center", padding: "20px"}}>
+            {/* <img src={docs[docIndex].image} style={{height: "100%"}}/> */}
+            <ImageViewer
+              src={photoSrc}
+            />
+          </div>
         </div>
-        <div style={{width: "40%", display: "flex", alignItems: "center", justifyContent: "center", padding: "20px"}}>
-          {/* <img src={docs[docIndex].image} style={{height: "100%"}}/> */}
-          <ImageViewer
-            src={photoSrc}
-          />
+        <div style={{width: "50%", display: "flex", flexDirection: "column", alignItems: "center", justifyContent: "center"}}>
+          <div>
+            <div style={{fontSize: "18px", fontWeight: "bold", marginBottom: "25px"}}>
+              На представленных изображения один и тот же человек?
+            </div>
+            <div style={{display: "flex", width: "100%", justifyContent: "center"}}>
+              <Button onClick={onNextStep} variant="contained" style={{width: "160px", backgroundColor: "green", padding: "15px 30px", marginRight: "15px", fontSize: "16px"}}>Да</Button>
+              <Button  onClick={onNextForm} variant="contained" style={{width: "160px", backgroundColor: "red", padding: "15px 30px", fontSize: "16px"}}>Нет</Button>
+            </div>
+          </div>
         </div>
       </div>
-      <div style={{width: "50%", display: "flex", flexDirection: "column", alignItems: "center", justifyContent: "center"}}>
-        <div>
-          <div style={{fontSize: "18px", fontWeight: "bold", marginBottom: "25px"}}>
-            На представленных изображения один и тот же человек?
-          </div>
-          <div style={{display: "flex", width: "100%", justifyContent: "center"}}>
-            <Button onClick={onNextStep} variant="contained" style={{width: "160px", backgroundColor: "green", padding: "15px 30px", marginRight: "15px", fontSize: "16px"}}>Да</Button>
-            <Button  onClick={onNextForm} variant="contained" style={{width: "160px", backgroundColor: "red", padding: "15px 30px", fontSize: "16px"}}>Нет</Button>
-          </div>
-        </div>
+      <div style={{display: "flex", justifyContent: "flex-end", paddingTop: "70px"}}>
+        <Button variant="contained">Завершить смену</Button>
       </div>
     </div>
   )

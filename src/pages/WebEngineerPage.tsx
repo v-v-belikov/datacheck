2854c20bb5a44e5f986d7005f1f047074987a0a8
Vladimir Belikov <v.v.belikov.work@gmail.com>
2024-06-18 18:06:25 +0400

    add select to web-engineer

--- src/pages/WebEngineerPage.tsx
+++ src/pages/WebEngineerPage.tsx
@@ -91,108 +91,129 @@ const initialRows = [
 
 export const WebEngineerPage = () => {
   const [rows, setRows] = useState(initialRows)
+  const [selectValue, setSelectValue] = useState("")
+
   return (
-    <div style={{paddingTop: "30px"}}>
+    <div style={{width: "750px", paddingTop: "30px"}}>
       <h1>Cписок полей кабинета проверки данных</h1>
-      <TableContainer component={Paper}>
-          <Table sx={{ minWidth: 750 }} aria-label="simple table">
-            <TableHead>
-              <TableRow>
-                {headers.map(header => (
-                  <TableCell align="left">{header}</TableCell>
-                ))}
-              </TableRow>
-            </TableHead>
-            <TableBody>
-              {rows.map((row, index) => (
-                <TableRow
-                  key={row.docFieldName}
-                  sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-                >
-                  <TableCell align="left"><Checkbox/></TableCell>
-                  <TableCell align="left">{row.docFieldName}</TableCell>
-                  <TableCell align="left">
-                    <Box sx={{ minWidth: 120 }}>
-                      <FormControl fullWidth>
-                        <InputLabel id="demo-simple-select-label">Поле</InputLabel>
-                        <Select
-                          // labelId="demo-simple-select-label"
-                          // id="demo-simple-select"
-                          // value={formFields[0]}
-                          value={rows[index].formFieldName}
-                          label="Поле"
-                          onChange={(evt) => {
-                            // consol
-                            const updatedRows = update(rows, {
-                              [index]: {
-                                formFieldName: {
-                                  $set: evt.target.value
-                                }
-                              }
-                            })
-                            setRows(updatedRows)
-                          }}
-                        >
-                          {
-                            formFields.map(field => (
-                              <MenuItem value={field}>{field}</MenuItem>
-                            ))
-                          }
-                        </Select>
-                      </FormControl>
-                    </Box>
-                  </TableCell>
-                </TableRow>
-              ))}
-            </TableBody>
-          </Table>
-        </TableContainer>
-        <div style={{display: "flex", justifyContent: "flex-end", paddingTop: "20px"}}>
-          <Button style={{padding: "10px 20px"}} variant="contained">Сохранить изменения</Button>
-        </div>
-      {/* <div style={{
-        display: "flex",
-        alignItems: "center",
-    }}>
-        <Checkbox
-          style={{
-            marginRight: "40px"
-          }}
-        />
-        <span style={{
-          marginRight: "40px"
-        }}>
-          Фамилия
-        </span> */}
-        {/* <Select
+      <FormControl style={{minWidth: "300px"}}>
+        <InputLabel id="demo-simple-select-label">Проект</InputLabel>
+        <Select
           labelId="demo-simple-select-label"
           id="demo-simple-select"
-          label="Поле"
-          value="nameRussia"
-          onChange={() => {}}
+          value={selectValue}
+          label="Проект"
+          onChange={(evt) => setSelectValue(evt.target.value)}
         >
-          
-        </Select> */}
-        {/* <Box sx={{ minWidth: 120 }}>
-          <FormControl fullWidth>
-            <InputLabel id="demo-simple-select-label">Поле</InputLabel>
-            <Select
-              labelId="demo-simple-select-label"
-              id="demo-simple-select"
-              // value={formFields[0]}
-              value="nameRussia"
-              label="Поле"
-              onChange={() => {}}
-            >
-              {
-                formFields.map(field => (
-                  <MenuItem value={10}>{field}</MenuItem>
-                ))
-              }
-            </Select>
-          </FormControl>
-        </Box> */}
-      {/* </div> */}
+          <MenuItem value={"Осенняя собачья олимпиада"}>Осенняя собачья олимпиада</MenuItem>
+          <MenuItem value={"Чемпионат России по кёрлингу"}>Чемпионат России по кёрлингу</MenuItem>
+          <MenuItem value={"Форум Россия"}>Форум Россия</MenuItem>
+        </Select>
+      </FormControl>
+      {selectValue && (
+            <div style={{paddingTop: "30px"}}>
+            <TableContainer component={Paper}>
+                <Table sx={{ minWidth: 750 }} aria-label="simple table">
+                  <TableHead>
+                    <TableRow>
+                      {headers.map(header => (
+                        <TableCell align="left">{header}</TableCell>
+                      ))}
+                    </TableRow>
+                  </TableHead>
+                  <TableBody>
+                    {rows.map((row, index) => (
+                      <TableRow
+                        key={row.docFieldName}
+                        sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                      >
+                        <TableCell align="left"><Checkbox/></TableCell>
+                        <TableCell align="left">{row.docFieldName}</TableCell>
+                        <TableCell align="left">
+                          <Box sx={{ minWidth: 120 }}>
+                            <FormControl fullWidth>
+                              <InputLabel id="demo-simple-select-label">Поле</InputLabel>
+                              <Select
+                                // labelId="demo-simple-select-label"
+                                // id="demo-simple-select"
+                                // value={formFields[0]}
+                                value={rows[index].formFieldName}
+                                label="Поле"
+                                onChange={(evt) => {
+                                  // consol
+                                  const updatedRows = update(rows, {
+                                    [index]: {
+                                      formFieldName: {
+                                        $set: evt.target.value
+                                      }
+                                    }
+                                  })
+                                  setRows(updatedRows)
+                                }}
+                              >
+                                {
+                                  formFields.map(field => (
+                                    <MenuItem value={field}>{field}</MenuItem>
+                                  ))
+                                }
+                              </Select>
+                            </FormControl>
+                          </Box>
+                        </TableCell>
+                      </TableRow>
+                    ))}
+                  </TableBody>
+                </Table>
+              </TableContainer>
+              <div style={{display: "flex", justifyContent: "flex-end", paddingTop: "20px"}}>
+                <Button style={{padding: "10px 20px"}} variant="contained">Сохранить изменения</Button>
+              </div>
+            {/* <div style={{
+              display: "flex",
+              alignItems: "center",
+          }}>
+              <Checkbox
+                style={{
+                  marginRight: "40px"
+                }}
+              />
+              <span style={{
+                marginRight: "40px"
+              }}>
+                Фамилия
+              </span> */}
+              {/* <Select
+                labelId="demo-simple-select-label"
+                id="demo-simple-select"
+                label="Поле"
+                value="nameRussia"
+                onChange={() => {}}
+              >
+                
+              </Select> */}
+              {/* <Box sx={{ minWidth: 120 }}>
+                <FormControl fullWidth>
+                  <InputLabel id="demo-simple-select-label">Поле</InputLabel>
+                  <Select
+                    labelId="demo-simple-select-label"
+                    id="demo-simple-select"
+                    // value={formFields[0]}
+                    value="nameRussia"
+                    label="Поле"
+                    onChange={() => {}}
+                  >
+                    {
+                      formFields.map(field => (
+                        <MenuItem value={10}>{field}</MenuItem>
+                      ))
+                    }
+                  </Select>
+                </FormControl>
+              </Box> */}
+            {/* </div> */}
+          </div>
+      )}
     </div>
+
   )
 }
